9765cc28bf01b72f77d1838ad3749a0eaaf2cf2b
andyrift <ahramigamedev@yandex.com>
2024-05-11 16:59:43 +0300

    style: add commas to colors.ts

--- src/editor/colors.ts
+++ src/editor/colors.ts
@@ -1,47 +1,47 @@
 type Color = {
   name: string, 
-  class: string
+  class: string,
 }
 
 export const colors : { [id: string] : Color } = {
   'none': {
     name: "Default",
-    class: "transparent"
+    class: "transparent",
   },
   'red': {
     name: "Red",
-    class: "red-300"
+    class: "red-300",
   },
   'orange': {
     name: "Orange",
-    class: "orange-300"
+    class: "orange-300",
   },
   'yellow': {
     name: "Yellow",
-    class: "yellow-300"
+    class: "yellow-300",
   },
   'green': {
     name: "Green",
-    class: "green-300"
+    class: "green-300",
   },
   'cyan': {
     name: "Cyan",
-    class: "cyan-300"
+    class: "cyan-300",
   },
   'blue': {
     name: "Blue",
-    class: "blue-300"
+    class: "blue-300",
   },
   'purple': {
     name: "Purple",
-    class: "purple-300"
+    class: "purple-300",
   },
   'pink': {
     name: "Pink",
-    class: "pink-300"
+    class: "pink-300",
   },
   'gray': {
     name: "Gray",
-    class: "gray-300"
+    class: "gray-300",
   },
 }
