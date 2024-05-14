23aa7306823e48807432ac7e7c25810086163908
andyrift <ahramigamedev@yandex.com>
2024-05-14 19:04:41 +0300

    feat(sidebar): make browsers collapsable

--- src/gui/components/sidebar/Sidebar.vue
+++ src/gui/components/sidebar/Sidebar.vue
@@ -8,26 +8,28 @@
         </div>
         <div class="grow"></div>
         <i class="fa-solid fa-angles-left cursor-pointer mr-3 hover:bg-zinc-200 my-1 p-2 rounded"
-          @click="pubSub.emit('toggle-sidebar', false)"></i>
+          @click.stop="handleToggleSidebar"></i>
       </div>
       <div class="divide-y divide-zinc-300 h-full flex flex-col">
-        <div
+        <div @click.stop="handleTogglePageBrowser"
           class="px-2 py-1 w-full overflow-x-hidden text-sm font-medium text-gray-900 hover:bg-gray-200 text-left outline-none flex-none select-none cursor-pointer">
-          <i class="fa-solid fa-chevron-down mr-2"></i>
+          <i v-if="showContainers.pageBrowser" class="fa-solid fa-chevron-down mr-2"></i>
+          <i v-else class="fa-solid fa-chevron-right mr-2"></i>
           <span>Page Browser</span>
         </div>
-        <div class="sidebar h-full w-full overflow-y-auto bg-zinc-100">
+        <div v-show="showContainers.pageBrowser" class="sidebar grow w-full overflow-y-auto bg-zinc-100">
           <PageBrowser />
         </div>
         <div class="border-t border-zinc-300">
-          <div
+          <div @click.stop="handleToggleTrashBrowser"
             class="px-2 py-1 w-full overflow-x-hidden text-sm font-medium text-gray-900 hover:bg-gray-200 text-left outline-none flex-none select-none cursor-pointer">
-            <i class="fa-solid fa-chevron-down mr-2"></i>
+            <i v-if="showContainers.trashBrowser" class="fa-solid fa-chevron-down mr-2"></i>
+            <i v-else class="fa-solid fa-chevron-right mr-2"></i>
             <i class="fa-solid fa-trash-can mr-2"></i>
             <span>Trash can</span>
           </div>
         </div>
-        <div class="sidebar h-full w-full overflow-y-auto bg-zinc-100">
+        <div v-show="showContainers.trashBrowser" class="sidebar grow w-full overflow-y-auto bg-zinc-100">
           <TrashBrowser />
         </div>
       </div>
@@ -54,6 +56,23 @@ const transitionWidth = ref(300)
 const transition = new FixedSquaredTransition((value) => {
   transitionWidth.value = value
 }, 0.5, 0, width.value)
+
+function handleToggleSidebar(e: MouseEvent) {
+  pubSub.emit('toggle-sidebar', false)
+}
+
+const showContainers = ref({
+  pageBrowser: true,
+  trashBrowser: false,
+})
+
+function handleTogglePageBrowser(e: MouseEvent) {
+  showContainers.value.pageBrowser = !showContainers.value.pageBrowser
+}
+
+function handleToggleTrashBrowser(e: MouseEvent) {
+  showContainers.value.trashBrowser = !showContainers.value.trashBrowser
+}
 
 pubSub.subscribe("toggle-sidebar-end", (value: boolean) => {
   if (!value) show.value = false
