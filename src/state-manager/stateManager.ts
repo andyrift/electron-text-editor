3f23590c102c5ea161e06f544326db66c2e5d68a
andyrift <ahramigamedev@yandex.com>
2024-05-15 01:58:45 +0300

    feat: add StateManager interface and partial realization

--- src/state-manager/stateManager.ts
+++ src/state-manager/stateManager.ts
@@ -1,10 +1,64 @@
+import { PageData } from "@src/database/model";
+import { Editor } from "@src/editor";
+import { PubSub } from "@src/pubSub";
 
-class StateManager {
+export interface IStateManager {
+  switchToPage(id: number): boolean
+}
+
+export class StateManager implements IStateManager {
+
+  pubSub: PubSub
+  editor: Editor
+
+  private current: number | null = null
+
+  private busy: boolean = true
+
   constructor() {
-
+    this.pubSub = PubSub.getInstance()
+    this.pubSub.subscribe('use-editor', () => {
+      this.init()
+    })
+    this.editor = Editor.getInstance()
   }
 
-  init() {
+  private async init() {
 
+    // here editor already has view
+    if (!this.editor.hasView()) throw "No editor view on StateManager init()"
+
+    if (await this.openPageInEditor(1))
+      this.editor.setEditable(true)
+
+    this.busy = false
+  }
+
+  private async openPageInEditor(id: number): Promise<boolean> {
+    let res = await window.invoke("db:getPageData", id)
+    if (!res.status) return false
+
+    this.current = res.value.id
+    this.editor.putPage(res.value.document, res.value.editor_state)
+    return true
+  }
+
+  getCurrentPageData(): PageData | null {
+    if (this.current === null) return null
+
+    const editorPage = this.editor.getPageData()
+    if (!editorPage) return null
+
+    const pageData: PageData = {
+      id: this.current,
+      document: editorPage.document,
+      editor_state: editorPage.editor_state,
+    }
+
+    return pageData
+  }
+
+  switchToPage(id: number): boolean {
+    return false
   }
 }
